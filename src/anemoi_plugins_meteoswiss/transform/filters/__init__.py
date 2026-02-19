9dfdba6ea748d78ae8f51cd255b4e9cfd78781c2
Francesco Zanetta <62377868+frazane@users.noreply.github.com>
2026-02-19 17:50:28 +0100

    Remove dependency on data-provider (#20)
    This PR removes the dependency on data-provider, since it will no longer be maintained.

--- src/anemoi_plugins_meteoswiss/transform/filters/__init__.py
+++ src/anemoi_plugins_meteoswiss/transform/filters/__init__.py
@@ -2,9 +2,6 @@ from .clipping import ClipLateralBoundaries
 from .destaggering import Destagger
 from .geopotential_from_height import GeopotentialFromHeight
 from .grid import AssignGrid
-from .horizontal_interpolation import Interp2Grid
-from .horizontal_interpolation import Interp2Res
-from .horizontal_interpolation import InterpNAFilter
 from .omega_from_w import OmegaFromW
 from .vertical_interpolation import InterpK2P
 

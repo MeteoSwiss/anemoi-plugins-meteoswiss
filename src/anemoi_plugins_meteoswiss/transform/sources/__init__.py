ac0ae1282d5f82f1f777b3f88672c2c13f130eeb
Francesco Zanetta <62377868+frazane@users.noreply.github.com>
2026-03-11 17:32:45 +0100

    Add custom source for REA-L-CH1 wind gust (#23)
    Currently, anemoi-datasets does not provide a general way to compute aggregations/reductions over the time dimension (the "accumulate" source is specifically for cumulative quantities, but cannot be used if the reduction is e.g. mean or max). For wind gust, we want to include hourly maxima in our dataset, but REA-L-CH1 only has maxima over the last 10 minutes. Therefore, we need a way to do compute the hourly maxima on-the-fly. The simplest solution is to implement a custom source.
    
    Feature request for a generic "reduce" source is here ecmwf/anemoi-datasets#567

--- src/anemoi_plugins_meteoswiss/transform/sources/__init__.py
+++ src/anemoi_plugins_meteoswiss/transform/sources/__init__.py
@@ -1,11 +1,7 @@
 from .realch1_precip import ReaLCh1Precip
+from .realch1_wind_gust import ReaLCh1WindGust
 
 __all__ = [
     "ReaLCh1Precip",
-    "NASADEM",
-    "OPERA",
-    "SATELLITE",
-    "SURFACE",
-    "INCA",
-    "ICON_CH1",
+    "ReaLCh1WindGust",
 ]

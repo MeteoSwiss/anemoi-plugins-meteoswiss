a62b9b9110cc0d9c8e4ce6c83c25b038d8efdd23
Luca Lanzilao <llanzila@balfrin-ln003.cscs.ch>
2026-07-31 11:35:36 +0200

    fix pre commit

--- src/anemoi_plugins_meteoswiss/transform/filters/__init__.py
+++ src/anemoi_plugins_meteoswiss/transform/filters/__init__.py
@@ -1,6 +1,6 @@
 from .geopotential_from_height import GeopotentialFromHeight
-from .nudging import NudgeTowardObservation
 from .grid import AssignGrid
+from .nudging import NudgeTowardObservation
 from .time_processing import AverageFluxToCumulativeQuantity
 from .vertical_interpolation import ModelToPressureLevel
 
